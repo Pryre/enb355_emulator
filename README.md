7dbaf5f5ada452087e106fff043c5d09bf2d08d7
pryre <kyemorton@hotmail.com>
2017-07-14 17:30:23 +1000

    Updates with pose and occupancy grid

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# enb355_emulator
+# uavtaq_emulator
 Small ROS publisher to output dummy data in the form of images, pose messages, and sensor readings
 
 This node will publish the following types:
--- src/uavtaq_emulator_node.cpp
+++ src/uavtaq_emulator_node.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <std_msgs/Int32.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/OccupancyGrid.h>
 #include <image_transport/image_transport.h>
 
 #include <string.h>
@@ -15,59 +17,42 @@ int main( int argc, char **argv ) {
 	//==== Set up variables ====//
 	std::string image_topic = "image_raw";
 	std::string transform_topic = "transform";
+	std::string pose_topic = "transform";
+	std::string grid_topic = "grid_map";
 	std::string gas_a_topic = "gas/a";
 	std::string gas_b_topic = "gas/b";
 	std::string gas_c_topic = "gas/c";
 
 	//==== Get parameters if set ====//
-	if( !nh.getParam( "image_topic", image_topic ) ) {
-		ROS_WARN( "No parameter set for \"image_topic\", using: %s", image_topic.c_str() );
-	} else {
-		ROS_INFO( "Publishing image to: %s", image_topic.c_str() );
-	}
-
-	if( !nh.getParam( "transform_topic", transform_topic ) ) {
-		ROS_WARN( "No parameter set for \"transform_topic\", using: %s", transform_topic.c_str() );
-	} else {
-		ROS_INFO( "Publishing transform to: %s", transform_topic.c_str() );
-	}
-
-	if( !nh.getParam( "gas_a_topic", gas_a_topic ) ) {
-		ROS_WARN( "No parameter set for \"gas_a_topic\", using: %s", gas_a_topic.c_str() );
-	} else {
-		ROS_INFO( "Publishing gas data (a) to: %s", gas_a_topic.c_str() );
-	}
-
-	if( !nh.getParam( "gas_b_topic", gas_b_topic ) ) {
-		ROS_WARN( "No parameter set for \"gas_b_topic\", using: %s", gas_b_topic.c_str() );
-	} else {
-		ROS_INFO( "Publishing gas data (b) to: %s", gas_b_topic.c_str() );
-	}
-
-	if( !nh.getParam( "gas_c_topic", gas_c_topic ) ) {
-		ROS_WARN( "No parameter set for \"gas_c_topic\", using: %s", gas_c_topic.c_str() );
-	} else {
-		ROS_INFO( "Publishing gas data (c) to: %s", gas_c_topic.c_str() );
-	}
+	nh_.param( "topic_image", image_topic, image_topic );
+	nh_.param( "topic_transform", image_topic, image_topic );
+	nh_.param( "topic_pose", pose_topic, pose_topic );
+	nh_.param( "topic_occupancy_grid", grid_topic, grid_topic );
+	nh_.param( "topic_gas_a", gas_a_topic, gas_a_topic);
+	nh_.param( "topic_gas_b", gas_b_topic, gas_b_topic);
+	nh_.param( "topic_gas_c", gas_c_topic, gas_c_topic);
 
 	//==== Begin publisher & subscriber ====//
 	ros::Publisher trans_pub = nh.advertise<geometry_msgs::TransformStamped>(transform_topic, 100);
+	ros::Publisher pose_pub = nh.advertise<geometry_msgs::PoseStamped>(pose_topic, 100);
 	ros::Publisher gas_a_pub = nh.advertise<std_msgs::Int32>(gas_a_topic, 10);
 	ros::Publisher gas_b_pub = nh.advertise<std_msgs::Int32>(gas_b_topic, 10);
 	ros::Publisher gas_c_pub = nh.advertise<std_msgs::Int32>(gas_c_topic, 10);
 
+	ros::Publisher grid_pub = nh.advertise<nav_msgs::OccupancyGrid>(grid_topic, 1, true);
+
 	image_transport::ImageTransport it(nh);
 	image_transport::Publisher img_pub = it.advertise(image_topic, 1);
-	//cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-	//cv::waitKey(30);
 
-	//Loop
-	ros::Rate loop_rate(100);
+	//Loop Config
+	ros::Rate loop_rate(50);
+
 	//Gas
 	int gas_rate_counter = 0;
 	std_msgs::Int32 gas_a_out;
 	std_msgs::Int32 gas_b_out;
 	std_msgs::Int32 gas_c_out;
+
 	//Image
 	int img_seq = 0;
 	int img_rate_counter = 0;
@@ -130,7 +115,7 @@ int main( int argc, char **argv ) {
 	//Transform
 	int numStep = 1000;
 	int trans_seq = 0;
-	double movementStep = 2*M_PI/numStep;
+	double movementStep = 2 * M_PI / numStep;
 	int currentStep = 0;
 	geometry_msgs::TransformStamped trans_out;
 	trans_out.header.frame_id = "world";
@@ -141,11 +126,44 @@ int main( int argc, char **argv ) {
 	trans_out.transform.rotation.y = 0;
 	trans_out.transform.rotation.z = 0;
 
+	//Pose
+	geometry_msgs::TransformStamped pose_out;
+	pose_out.header.frame_id = trans_out.header.frame_id;
+	pose_out.pose.position.z = trans_out.transform.translation.z;
+	pose_out.pose.orientation.w = trans_out.transform.rotation.w;
+	pose_out.pose.orientation.x = trans_out.transform.rotation.x;
+	pose_out.pose.orientation.y = trans_out.transform.rotation.y;
+	pose_out.pose.orientation.z = trans_out.transform.rotation.z;
+
+	//Occupancy Grid
+	ros::Time grid_stamp = ros::Time::now();
+	nav_msgs::OccupancyGrid grid_out;
+	grid_out.header.frame_id = "world";
+	grid_out.header.stamp = grid_stamp;
+	grid_out.info.map_load_time = grid_stamp;
+	grid_out.info.resolution = 0.1;
+	grid_out.info.width = 50;
+	grid_out.info.height = 50;
+	grid_out.info.origin.position.x = -2.5;
+	grid_out.info.origin.position.y = -2.5;
+	grid_out.info.origin.orientation.w = 1.0;
+
+	for(int gv = 0; gv < ( grid_out.info.width * grid_out.info.height ); gv++) {
+		int8_t val = -1;
+
+		if(std::rand() > 0.5) {
+			val = 100;
+		}
+
+		grid_out.data.push_back(val);
+	}
+
+	grid_pub.publish(grid_out);
 
 	//==== Begin publishing topics ====//
 	while ( ros::ok() ) {
 		//Gas
-		if( gas_rate_counter >= 20 ) {
+		if( gas_rate_counter >= 10 ) {
 			gas_a_out.data = std::rand() % 10 + 1;
 			gas_b_out.data = std::rand() % 200 + 200;
 			gas_c_out.data = std::rand() % 50 + 400;
@@ -160,8 +178,7 @@ int main( int argc, char **argv ) {
 		gas_rate_counter++;
 
 		//Image
-		if( img_rate_counter >= 50 ) {
-			img_seq++;
+		if( img_rate_counter >= 25 ) {
 			img_out.header.stamp = ros::Time::now();
 			img_out.header.seq = img_seq;
 
@@ -174,19 +191,25 @@ int main( int argc, char **argv ) {
 
 		//Transform
 		currentStep++;
-		trans_seq++;
 
 		if(currentStep > numStep) {
 			currentStep = 0;
 		}
 
 		trans_out.header.stamp = ros::Time::now();
-		trans_out.header.seq = trans_seq;
 
-		trans_out.transform.translation.x = 2*std::cos(currentStep*movementStep);
-		trans_out.transform.translation.y = 4*std::sin(currentStep*movementStep);
+		trans_out.transform.translation.x = 2 * std::cos( currentStep * movementStep );
+		trans_out.transform.translation.y = 4 * std::sin( currentStep * movementStep );
 
 		trans_pub.publish(trans_out);
+
+		//Pose
+		pose_out.header.stamp = ros::Time::now();
+
+		pose_out.pose.position.x = trans_out.transform.translation.x;
+		pose_out.pose.position.y = trans_out.transform.translation.y;
+
+		pose_pub.publish(pose_out);
 
 		//Sleep
 		loop_rate.sleep();
